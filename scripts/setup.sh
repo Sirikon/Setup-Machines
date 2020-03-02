c1369738f9ed9badce1894fd51b236fdfbdace87
Carlos Fdez. Llamas <hello@sirikon.me>
2020-03-02 17:54:49 +0100

    Improve install script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,54 +2,98 @@
 
 set -e
 
+SETUP_TEMP_FOLDER=/tmp/setup-script
+
 function start {
-	mkdir -p /tmp/setup-script
+	log-title "Starting"
+	create-temp-folder
+	log-sep
 	
-	title "Installing repositories"
-	apt install -y apt-transport-https
-	vscode-repo
-	sublime-repo
-	keepassxc-repo
-	apt update
+	log-title "Installing apt keys"
+	install-apt-key "https://download.sublimetext.com/sublimehq-pub.gpg"
+	install-apt-key "https://packages.microsoft.com/keys/microsoft.asc"
+	install-apt-key "https://dbeaver.io/debs/dbeaver.gpg.key"
+	log-sep
 
-	title "Priorizing repositories for packages"
-	touch /etc/apt/preferences.d/extra-settings
-	keepassxc-pin
-	apt update
+	log-title "Installing repositories"
+	install-apt-repository "sublime-text" 	"deb https://download.sublimetext.com/ apt/stable/"
+	install-apt-repository "vscode" 		"deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+	install-apt-repository "dbeaver" 		"deb https://dbeaver.io/debs/dbeaver-ce /"
+	install-ppa "ppa:phoerious/keepassxc"
+	log-sep
 
-	title "Installing packages"
-	apt install -y vim keepassxc gnome-shell-extension-appindicator sublime-merge sublime-text python3-gpg gnome-tweaks
+	log-title "Pinning packages"
+	pin-package "keepassxc" 	"release o=LP-PPA-phoerious-keepassxc"
+	pin-package "dbeaver-ce" 	"origin dbeaver.io"
+	log-sep
 
-	title "Installing Dropbox"
-	wget -O /tmp/setup-script/dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2019.02.14_amd64.deb
-	apt install -y /tmp/setup-script/dropbox.deb
+	log-title "Refreshing packages"
+	install-repository-addition-dependencies
+	log-sep
+	log-step "apt update..."
+	apt-get update > /dev/null
+	log-sep
 
-	title "Cleaning up"
-	rm -rf /tmp/setup-script
-	echo "Done!"
+	log-title "Installing packages"
+	apt-get install -y vim keepassxc gnome-shell-extension-appindicator sublime-merge sublime-text python3-gpg gnome-tweaks dbeaver-ce code
+	log-sep
+
+	# title "Installing Dropbox"
+	# wget -O /tmp/setup-script/dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2019.02.14_amd64.deb
+	# apt install -y /tmp/setup-script/dropbox.deb
+
+	log-title "Cleaning up"
+	clean-temp-folder
 }
 
-function sublime-repo {
-	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-	echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+function create-temp-folder {
+	log-step "Creating temporal folder for setup in '${SETUP_TEMP_FOLDER}'"
+	mkdir -p $SETUP_TEMP_FOLDER
 }
 
-function vscode-repo {
-	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/setup-script/microsoft.gpg
-	install -o root -g root -m 644 /tmp/setup-script/microsoft.gpg /etc/apt/trusted.gpg.d/
-	echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+function clean-temp-folder {
+	rm -rf $SETUP_TEMP_FOLDER
 }
 
-function keepassxc-repo {
-	add-apt-repository -y ppa:phoerious/keepassxc
+function install-repository-addition-dependencies {
+	log-step "Installing 'apt-transport-https'"
+	apt-get install -y apt-transport-https > /dev/null
 }
 
-function keepassxc-pin {
-	echo "Package: keepassxc\nPin: release o=LP-PPA-phoerious-keepassxc\nPin-Priority: 2000\n\n" > /etc/apt/preferences.d/extra-settings
+function install-apt-key {
+	log-step "Installing apt key '${1}'"
+	curl -s "${1}" | apt-key add -
 }
 
-function title {
+function install-apt-repository {
+	ID=${1}
+	CONF=${2}
+	log-step "Installing apt repository ${ID}"
+	printf "${CONF}\n" > /etc/apt/sources.list.d/${ID}.list
+}
+
+function install-ppa {
+	log-step "Installing ppa repository ${1}"
+	add-apt-repository -yn "${1}"
+}
+
+function pin-package {
+	PACKAGE_NAME=${1}
+	MATCHER=${2}
+	log-step "Pinning package ${PACKAGE_NAME}"
+	printf "Package: ${PACKAGE_NAME}\nPin: ${MATCHER}\nPin-Priority: 2000\n" > /etc/apt/preferences.d/pin-${PACKAGE_NAME}
+}
+
+function log-title {
 	echo "########################## ${1}"
+}
+
+function log-sep {
+	echo ""
+}
+
+function log-step {
+	echo "${1}"
 }
 
 start
