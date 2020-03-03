586801faf7aa498fdabbc29150eb14cefc63379e
Carlos Fdez. Llamas <hello@sirikon.me>
2020-03-03 13:15:32 +0100

    Colorization. Better sudo usage.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,7 @@ SETUP_TEMP_FOLDER=/tmp/setup-script
 
 function start {
 	log-title "Starting"
+	ensure-sudo
 	create-temp-folder
 	log-sep
 	
@@ -31,12 +32,17 @@ function start {
 	log-title "Refreshing packages"
 	install-repository-addition-dependencies
 	log-sep
-	log-step "apt update..."
-	apt-get update > /dev/null
+
+	log-step "apt update"
+	sudo apt-get update
+	log-sep
+
+	log-step "apt upgrade"
+	sudo apt-get upgrade -y
 	log-sep
 
 	log-title "Installing packages"
-	apt-get install -y vim keepassxc gnome-shell-extension-appindicator sublime-merge sublime-text python3-gpg gnome-tweaks dbeaver-ce code
+	sudo apt-get install -y vim keepassxc gnome-shell-extension-appindicator sublime-merge sublime-text python3-gpg gnome-tweaks dbeaver-ce code
 	log-sep
 
 	# title "Installing Dropbox"
@@ -47,54 +53,63 @@ function start {
 	clean-temp-folder
 }
 
+function ensure-sudo {
+	sudo -v
+}
+
 function create-temp-folder {
 	log-step "Creating temporal folder for setup in '${SETUP_TEMP_FOLDER}'"
 	mkdir -p $SETUP_TEMP_FOLDER
 }
 
 function clean-temp-folder {
+	log-step "Cleaning temporal folder in '${SETUP_TEMP_FOLDER}'"
 	rm -rf $SETUP_TEMP_FOLDER
 }
 
 function install-repository-addition-dependencies {
 	log-step "Installing 'apt-transport-https'"
-	apt-get install -y apt-transport-https > /dev/null
+	sudo apt-get install -y apt-transport-https
 }
 
 function install-apt-key {
 	log-step "Installing apt key '${1}'"
-	curl -s "${1}" | apt-key add -
+	curl -s "${1}" | sudo apt-key add -
 }
 
 function install-apt-repository {
 	ID=${1}
 	CONF=${2}
 	log-step "Installing apt repository ${ID}"
-	printf "${CONF}\n" > /etc/apt/sources.list.d/${ID}.list
+	printf "${CONF}\n" | sudo tee /etc/apt/sources.list.d/${ID}.list
 }
 
 function install-ppa {
 	log-step "Installing ppa repository ${1}"
-	add-apt-repository -yn "${1}"
+	sudo add-apt-repository -yn "${1}"
 }
 
 function pin-package {
 	PACKAGE_NAME=${1}
 	MATCHER=${2}
 	log-step "Pinning package ${PACKAGE_NAME}"
-	printf "Package: ${PACKAGE_NAME}\nPin: ${MATCHER}\nPin-Priority: 2000\n" > /etc/apt/preferences.d/pin-${PACKAGE_NAME}
+	printf "Package: ${PACKAGE_NAME}\nPin: ${MATCHER}\nPin-Priority: 2000\n" | sudo tee /etc/apt/preferences.d/pin-${PACKAGE_NAME}
+}
+
+function install-node-version-manager {
+	curl -L https://git.io/n-install | bash
 }
 
 function log-title {
-	echo "########################## ${1}"
+	echo -e "\e[96m=> \e[1m${1}\e[21m\e[0m"
+}
+
+function log-step {
+	echo -e "\e[36m==> ${1}\e[0m"
 }
 
 function log-sep {
 	echo ""
-}
-
-function log-step {
-	echo "${1}"
 }
 
 start
